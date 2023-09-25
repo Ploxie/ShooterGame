9cb9c02b5815d3a9716b93f46473cf282a1289fc
SniffeKliffe <56879864+SniffeKliffe@users.noreply.github.com>
2023-09-25 22:09:54 +0200

    Pop up
    - Text will pop up if come near "body"
        - Can't interact with it

---
+++ Assets/ScriptsPhilip/PopUpConsumable.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PopUpConsumable : MonoBehaviour
+{
+    public GameObject floatingTextPrefab;
+    private GameObject floatingTextInstance;
+
+    private void Start()
+    {
+        floatingTextInstance = Instantiate(floatingTextPrefab, transform.position, Quaternion.identity, transform);
+        floatingTextInstance.SetActive(false);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player")) {
+            floatingTextInstance.SetActive(true);
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player")){
+            floatingTextInstance.SetActive(false);
+        }
+    }
+}
