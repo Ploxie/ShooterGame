2252f292fde58afe7e2fccecdcd81b1e2b29728f
SniffeKliffe <56879864+SniffeKliffe@users.noreply.github.com>
2023-09-26 16:57:39 +0200

    Fixed UI for Pop Up cartridge
    - UI will pop up if near an object that has cartridge. If go out of object UI will disappear.

--- Assets/ScriptsPhilip/PopUpConsumable.cs
+++ Assets/ScriptsPhilip/PopUpConsumable.cs
@@ -10,7 +10,7 @@ public class PopUpConsumable : MonoBehaviour
 
     private void Start()
     {
-        floatingTextInstance = Instantiate(floatingTextPrefab, transform.position, Quaternion.identity, transform);
+        floatingTextInstance = Instantiate(floatingTextPrefab);
         floatingTextInstance.SetActive(false);
     }
 
