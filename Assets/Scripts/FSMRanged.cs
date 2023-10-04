ee99f9531e7dcdd8690ef72b4cac48fc3da35bdf
Edvper <edvin1.persson@hotmail.se>
2023-10-04 14:20:44 +0200

    Animations for ranged enemy

--- Assets/Scripts/FSMRanged.cs
+++ Assets/Scripts/FSMRanged.cs
@@ -18,7 +18,7 @@ public class FSMRanged : MonoBehaviour
     private State state;
     private EnemyRanged enemy;
     private Player player;
-    bool walkfwrd = false, walkbwrd = false, idle = true; 
+    bool walkfwrd = false, walkbwrd = false, idle = false, shangedIdle = false, shangedfwrd = false, shangedBackwads = false, changedPatrol = false;//animations 
     Animator animator;
     private Vector3 lastSeenPos;
     private bool shouldMoveToLastPosition;
@@ -27,47 +27,80 @@ public class FSMRanged : MonoBehaviour
     {
         animator = GetComponent<Animator>();
         shouldMoveToLastPosition = true;
-        player = EnemyManager.Instance.Player;
+        //player = FindObjectOfType<Player>();
+        
         enemy = GetComponent<EnemyRanged>();
     }
-
+    private void Awake()
+    {
+        player = FindObjectOfType<Player>();//EnemyManager.Instance.Player;
+    }
     private void Update()
     {
-        if (player == null)
-        {
-            player = EnemyManager.Instance.Player;
-        }
-        switch (state)//switch no work we will try the object oriented way. maybe done points on the map where we show where the enemy should be
-        {
-            case State.STATE_PATROL://go to last place of enemy and go back to original place. It uses a bool check to keep the patrol state at either look for enemy at a place
-                walkfwrd = true;
-                walkbwrd = false;
-                idle = false;
-                animator.SetBool("WalkForward", walkfwrd);//and will return to the original place of the enemy. Will change to shoot state when seeing player with raycast.   
+        //if (player == null)
+        //{
+            //player = EnemyManager.Instance.Player;
+        //}
+        //else{
+            switch (state)//switch no work we will try the object oriented way. maybe done points on the map where we show where the enemy should be
+            {
+                case State.STATE_PATROL://go to last place of enemy and go back to original place. It uses a bool check to keep the patrol state at either look for enemy at a place
+                if (changedPatrol == true)
+                {
+                    walkfwrd = false;
+                    walkbwrd = true;
+                    idle = false;
+                    changedPatrol = false;
+                    animator.SetBool("IdleState", idle);
+                    animator.SetBool("WalkForward", walkfwrd);
+                    animator.SetBool("WalkBackwards", walkbwrd);
+                }//and will return to the original place of the enemy. Will change to shoot state when seeing player with raycast.   
                 HandlePatrolState();
-                break;
-            case State.STATE_SHOOTING:
-                walkfwrd = false;
-                walkbwrd = false;
-                idle = true;
-                animator.SetBool("IdleState", idle);
+                    break;
+                case State.STATE_SHOOTING:
+                if (shangedIdle == true)
+                {
+                    walkfwrd = false;
+                    walkbwrd = false;
+                    idle = true;
+                    shangedIdle = false;
+                    animator.SetBool("IdleState", idle);
+                    animator.SetBool("WalkForward", walkfwrd);
+                    animator.SetBool("WalkBackwards", walkbwrd);
+                }    
+                
                 HandleShootingState();
-                break;
-            case State.STATE_RUNNING_TO_ENEMY_WHILE_SHOOTING://running towards enemy in this state.
-                walkfwrd = true;
-                walkbwrd = false;
-                idle = false;
-                animator.SetBool("WalkForward", walkfwrd);
+                    break;
+                case State.STATE_RUNNING_TO_ENEMY_WHILE_SHOOTING://running towards enemy in this state.
+                    
+                if (shangedfwrd == true)
+                {
+                    walkfwrd = true;
+                    walkbwrd = false;
+                    idle = false;
+                    shangedfwrd = false;
+                    animator.SetBool("IdleState", idle);
+                    animator.SetBool("WalkForward", walkfwrd);
+                    animator.SetBool("WalkBackwards", walkbwrd);
+                }
                 HandleRunningToEnemyWhileShootingState();
-                break;
-            case State.STATE_RUNNING_FROM_ENEMY_WHILE_SHOOTING://running from enemy in this state.
-                walkfwrd = false;
-                walkbwrd = true;
-                idle = false;
-                animator.SetBool("WalkBackwards", true);
-                HandleRunningFromEnemyWhileShootingState();
-                break;
-        }
+                    break;
+                case State.STATE_RUNNING_FROM_ENEMY_WHILE_SHOOTING://running from enemy in this state.
+                if (shangedBackwads == true)
+                {
+                    walkfwrd = false;
+                    walkbwrd = true;
+                    idle = false;
+                    shangedBackwads = false;
+                    animator.SetBool("IdleState", idle);
+                    animator.SetBool("WalkForward", walkfwrd);
+                    animator.SetBool("WalkBackwards", walkbwrd);
+                }
+                    HandleRunningFromEnemyWhileShootingState();
+                    break;
+            }
+        //}
+        
     }
        
     private void HandleRunningFromEnemyWhileShootingState()
@@ -81,12 +114,13 @@ public class FSMRanged : MonoBehaviour
             if (25f <= distBetweenEnemyPlayer)//move from player
             {
                 state = State.STATE_SHOOTING;
-                
+                shangedIdle = true;
             }
         }
         else
         {
             state = State.STATE_PATROL;
+            changedPatrol = true;
             lastSeenPos = player.transform.position;
         }
         print(state);
@@ -104,11 +138,13 @@ public class FSMRanged : MonoBehaviour
             if (10f < distBetweenEnemyPlayer && 25f > distBetweenEnemyPlayer)//move to player
             {
                 state = State.STATE_SHOOTING;
+                shangedIdle = true;
             }
         }
         else
         {
             state = State.STATE_PATROL;
+            shangedfwrd = true;
             lastSeenPos = player.transform.position;
         }
         print(state);
@@ -125,10 +161,12 @@ public class FSMRanged : MonoBehaviour
             if (seen == true)
             {
                 state = State.STATE_RUNNING_FROM_ENEMY_WHILE_SHOOTING;
+                shangedBackwads = true;
             }
             else
             {
                 state = State.STATE_PATROL;
+                changedPatrol = true;
                 lastSeenPos = player.transform.position;
             }
         }
@@ -137,11 +175,13 @@ public class FSMRanged : MonoBehaviour
             if (seen == true)
             {
                 state = State.STATE_RUNNING_TO_ENEMY_WHILE_SHOOTING;
+                shangedfwrd = true;
             }
             else
             {
                 state = State.STATE_PATROL;
                 lastSeenPos = player.transform.position;
+                changedPatrol= true;
             }
         }
         else
@@ -154,17 +194,14 @@ public class FSMRanged : MonoBehaviour
             {
                 state = State.STATE_PATROL;
                 lastSeenPos = player.transform.position;
+                changedPatrol = true;
             }
         }
     }
 
     private void HandlePatrolState()
     {
-        if (player == null)
-        {
-            player = EnemyManager.Instance.Player;
-        }
-        bool seen = enemy.HasLineOfSightTo(player.transform.position, "Player");
+        bool seen = enemy.HasLineOfSightTo(player.transform.position, player.gameObject.tag);
 
         if (seen != true)
         {
@@ -183,6 +220,7 @@ public class FSMRanged : MonoBehaviour
         else
         {
             state = State.STATE_SHOOTING;
+            changedPatrol = true;
             shouldMoveToLastPosition = false;
         }
     }
