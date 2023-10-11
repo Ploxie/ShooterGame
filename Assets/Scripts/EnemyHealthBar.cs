f9ff50617d70ec64b164d244411a446fe14e98dc
Zeradamuz <robin@pamo.se>
2023-10-11 13:15:10 +0200

    slow moving health bar

--- Assets/Scripts/EnemyHealthBar.cs
+++ Assets/Scripts/EnemyHealthBar.cs
@@ -8,22 +8,38 @@ public class EnemyHealthBar : MonoBehaviour
     // Start is called before the first frame update
     public Image enemyHealthBar;
     public Image enemyHealthBarBorder;
+    public Image enemyMovingHealthBar;
     private float enemyHealthAmount;
     public Transform target;
+    public float timer;
 
     Living living;
+    public float enemyMovingHealth;
     void Start()
     {
         target = GameObject.FindGameObjectWithTag("MainCamera").transform;
         living = GetComponentInParent<Living>();
         enemyHealthAmount = living.MaxHealth;
+        enemyMovingHealth = living.MaxHealth;
+        timer = 0;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
-
+        enemyMovingHealthBar.fillAmount = enemyMovingHealth / living.MaxHealth;
+        if (enemyMovingHealth > living.Health)
+        {
+            timer += Time.deltaTime;
+            if (timer > 0.5f)
+            {
+                enemyMovingHealth -= 0.25f;
+            }
+            if (enemyMovingHealth <= living.Health)
+            {
+                timer = 0;
+            }
+        }
         enemyHealthBarBorder.transform.LookAt(target);
     }
 
@@ -31,6 +47,7 @@ public class EnemyHealthBar : MonoBehaviour
     {
         enemyHealthAmount -= damage;
         enemyHealthBar.fillAmount = enemyHealthAmount / living.MaxHealth;
+        
     }
 
 }
--- Assets/Scripts/HealthBarManager.cs
+++ Assets/Scripts/HealthBarManager.cs
@@ -5,14 +5,16 @@ using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
 using UnityEngine.SceneManagement;
-
+using System.Threading;
 
 public class HealthBarManager : MonoBehaviour
 {
     public Image healthBar;
     public TMP_Text HealthText;
-
+    public Image movingHealthBar;
     private Player player;
+    public float movingHealth = 100;
+    public float timer = 0;
 
 
     // Update is called once per frame
@@ -21,9 +23,26 @@ public class HealthBarManager : MonoBehaviour
         if (EnemyManager.Instance.Player == null)
             return;
 
+        
         var player = EnemyManager.Instance.Player;
+        
 
         healthBar.fillAmount = player.Health / player.MaxHealth;
         HealthText.text = $"{player.Health}/{player.MaxHealth}";
+        movingHealthBar.fillAmount = movingHealth / player.MaxHealth;
+        if(movingHealth > player.Health)
+        {
+            timer += Time.deltaTime;
+            if(timer > 0.5f)
+            {
+                movingHealth -= 0.25f;
+            }
+            if(movingHealth <= player.Health)
+            {
+                timer = 0;
+            }
+        }
     }
+
+
 }
